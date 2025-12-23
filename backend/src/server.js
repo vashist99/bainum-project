8d1b4b47aa4189bfced8393c6a13dc9ac25473da
Vashist Hegde <vashistgh99@gmail.com>
2025-12-23 09:29:06 -0500

    adjusted cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ connectDB();
 
 //middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://bainum-project-saf2.vercel.app/",
 }));
 app.use(express.json());
 app.use(rateLimiter);
